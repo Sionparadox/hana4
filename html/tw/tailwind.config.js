1809bfa86a4ca3d34362b059ffd4ed0aeb30c5f6
Sionparadox <sion9999@naver.com>
2024-08-19 16:18:10 +0900

    tailwind start

---
+++ html/tw/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#3333ff',
+        danger: '#ff3333'
+      }
+    },
+  },
+  plugins: [],
+}
+
